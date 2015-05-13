57360530cf30f3b452543efd19a7424f6f3fd7e7
Martin Caslavsky <mcaslavsky@gmail.com>
2015-05-13 22:26:23 +0200

    add: index page

--- src/main/java/config/SpringWebConfig.java
+++ src/main/java/config/SpringWebConfig.java
@@ -2,10 +2,17 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("web")
-public class SpringWebConfig {
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>TBA</title>
+</head>
+<body>
+<h1>TBA</h1>
+</body>
+</html>
